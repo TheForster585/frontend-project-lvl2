29bc80cfaa016109842c451ffa6978027e052837
TheForster585 <Neonester@yandex.ru>
2022-03-14 10:47:32 +0300

    Add Makefile and help

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+//const { Command } = require('commander');
+
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1');
+
+program.parse();
+console.log('pipka');
